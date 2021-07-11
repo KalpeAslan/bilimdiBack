53a723164ff99346474c3cd14901452f7eb92850
KalpeAslan <79480776+KalpeAslan@users.noreply.github.com>
2021-07-11 21:04:25 +0600

    fix: subjects for profsValue

--- services/filter.js
+++ services/filter.js
@@ -1,15 +1,72 @@
 const db = require('../db/db');
 
 
+const subjectsList = ["ГеоБио", "Творч", "ФизМат", "БиоХим", "ГеоИст", "КазКЛит", "РусРЛит",
+    "ИноИст", "КазЛит/РусЛит", "МатГео", "ИстЧоп", "ФизХим", "ГеоИно"
+].map(subj => subj.toLowerCase());
 
+const isTrueSubj = (firstSubject, secondSubject) => subjectsList.includes(firstSubject.toLowerCase() + secondSubject.toLowerCase())
+    ? (firstSubject.toLowerCase() + secondSubject.toLowerCase()) :
+    (secondSubject.toLowerCase() + firstSubject.toLowerCase())
 
-function isTrueSubj(firstSubject, secondSubject) {
-    const subjectsList = ["ГеоБио", "Творч", "ФизМат", "БиоХим", "ГеоИст", "КазКЛит", "РусРЛит",
-        "ИноИст", "КазЛит/РусЛит", "МатГео", "ИстЧоп", "ФизХим", "ГеоИно"
-    ].map(subj => subj.toLowerCase());
-    return subjectsList.includes(firstSubject.toLowerCase() + secondSubject.toLowerCase()) ? (firstSubject.toLowerCase() + secondSubject.toLowerCase()) :
-        (secondSubject.toLowerCase() + firstSubject.toLowerCase())
-}
+const subjectsLongList = [
+    {
+        short: 'геобио',
+        long: 'География/Биология'
+    },
+    {
+        short: 'творч',
+        long: 'Творческий экзамен'
+    },
+    {
+        short: 'физмат',
+        long: 'Физика/Математика'
+    },
+
+    {
+        short: 'БиоХим',
+        long: 'Биология/Химия'
+    },
+    {
+        short: 'ГеоИст',
+        long: 'География/Всемирная история'
+    },
+    {
+        short: 'КазКЛит',
+        long: 'Казахский язык/Казахская литература'
+    },
+    {
+        short: 'РусРЛит',
+        long: 'Русский язык/Русская литература'
+    },
+    {
+        short: 'казлит/руслит',
+        long: 'Казахская литература/Русская литература'
+    },
+    {
+        short: 'МатГео',
+        long: 'Математика/География'
+    },
+    {
+        short: 'ИстЧоп',
+        long: 'Всемирная история/Человек.Общество.Право'
+    },
+    {
+        short: 'ИноИст',
+        long: 'Иностранный язык/Человек.Общество.Право'
+    },
+    {
+        short: 'ФизХим',
+        long: 'Физика/Химия'
+    },
+    {
+        short: 'ГеоИно',
+        long: 'География/Иностранный язык'
+    }
+]
+
+const getLongVersionOfSubjectsByShort = (rawSubjects) => subjectsLongList.filter(subject => subject.short.toLowerCase() === rawSubjects)[0].long
+
 
 class Filter {
     _getAllProfs() {
@@ -41,6 +98,7 @@ class Filter {
         return Object.entries(db).reduce((acc, [subjectKey, subjectValue]) => {
             Object.entries(subjectValue).forEach(([branchKey, branchValue]) => {
                 Object.entries(branchValue).forEach(([profKey, profValue]) => {
+                    profValue.subjects = getLongVersionOfSubjectsByShort(subjectKey)
                     acc.push(profValue)
                 })
             })
@@ -68,12 +126,18 @@ class Filter {
                     return subjectKey.toLowerCase().includes(subj1 + subj2)
                 }
                 if (isSubjInlcudes(firstSubjectShort, secondSubjectShort) || isSubjInlcudes(secondSubjectShort, firstSubjectShort)) {
-                    Object.entries(subjectValue).forEach(([branchKey, branchValue]) => Object.entries(branchValue).forEach(([profKey, profValue]) => acc.push(profValue)))
+                    Object.entries(subjectValue).forEach(([branchKey, branchValue]) => Object.entries(branchValue).forEach(([profKey, profValue]) => {
+                        profValue.subjects = getLongVersionOfSubjectsByShort(subjectKey)
+                        acc.push(profValue)
+                    }))
                 }
             } else {
                 [firstSubject, secondSubject].filter(subj => subj !== null).forEach(subj => {
                     if (subjectKey.toLowerCase().includes(subj.toLowerCase())) {
-                        Object.entries(subjectValue).forEach(([branchKey, branchValue]) => Object.entries(branchValue).forEach(([profKey, profValue]) => acc.push(profValue)))
+                        Object.entries(subjectValue).forEach(([branchKey, branchValue]) => Object.entries(branchValue).forEach(([profKey, profValue]) => {
+                            profValue.subjects = getLongVersionOfSubjectsByShort(subjectKey)
+                            acc.push(profValue)
+                        }))
                     }
                 })
             }
@@ -89,7 +153,7 @@ class Filter {
                     const firstSubjectShort = firstSubject.toLowerCase()
                     if (firstSubject && !secondSubject && subjectKey.includes(firstSubjectShort)) return acc.push(prof)
                     if (firstSubject && secondSubject && (subjectKey === firstSubjectShort + secondSubject.toLowerCase() ||
-                            subjectKey === secondSubject.toLowerCase() + firstSubjectShort)) return acc.push(prof)
+                        subjectKey === secondSubject.toLowerCase() + firstSubjectShort)) return acc.push(prof)
                 })
             })
             return acc
